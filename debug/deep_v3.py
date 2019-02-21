848c8d2ddd8e030e653c376c269295a5156bce39
neocruiser <slei.bass@gmail.com>
2019-02-21 10:53:32 -0500

    import kaggle kernel to the hub

---
+++ debug/deep_v3.py
@@ -0,0 +1,74 @@
+## embedding with neural network
+## keras starter NN with embeddings
+## lightgbm fixing unbalanced data
+## RNN with keras ridge SGDR
+
+
+import pandas as pd
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+import os
+os.environ['OMP_NUM_THREADS'] = '4'
+import gc
+
+
+## testing dir
+path = '../input/'
+
+
+train_df = pd.read_csv(path+"train.csv",l
+                       dtype=dtypes,
+                       skiprows = range(1, 131886954),
+                       usecols=['ip',
+                                'app',
+                                'device',
+                                'os',
+                                'channel',
+                                'click_time',
+                                'is_attributed'])
+print('load test....')
+test_df = pd.read_csv(path+"test.csv",
+                      dtype=dtypes,
+                      usecols=['ip',
+                               'app',
+                               'device',
+                               'os',
+                               'channel',
+                               'click_time',
+                               'click_id'])
+
+
+len_train = len(train_df)
+train_df=train_df.append(test_df)
+del test_df; gc.collect()
+
+
+from keras.layers import Input, Embedding, Dense, Flatten, Dropout, concatenate
+from keras.layers import BatchNormalization, SpatialDropout1D
+from keras.callbacks import Callback
+from keras.models import Model
+from keras.optimizers import Adam
+emb_n = 50
+dense_n = 1000
+
+batch_size = 20000
+epochs = 2
+exp_decay = lambda init, fin, steps: (init/fin)**(1/(steps-1)) - 1
+steps = int(len(train_df) / batch_size) * epochs
+lr_init, lr_fin = 0.001, 0.0001
+lr_decay = exp_decay(lr_init, lr_fin, steps)
+optimizer_adam = Adam(lr=0.001, decay=lr_decay)
+model.compile(loss='binary_crossentropy',optimizer=optimizer_adam,metrics=['accuracy'])
+
+model.summary()
+
+model.fit(train_df, y_train, batch_size=batch_size, epochs=2, shuffle=True, verbose=2)
+del train_df, y_train; gc.collect()
+model.save_weights('dl_support.h5')
+
+print("predicting....")
+sub['is_attributed'] = model.predict(test_df, batch_size=batch_size, verbose=2)
+del test_df; gc.collect()
+print("writing....")
+sub.to_csv('dl_support.csv',index=False)
---
+++ debug/reinforce_v2.py
@@ -0,0 +1,209 @@
+import numpy as np
+import pandas as pd
+import time
+import copy
+import numpy as np
+import pandas as pd
+import chainer
+import chainer.functions as F
+import chainer.links as L
+from plotly import tools
+from plotly.graph_objs import *
+from plotly.offline import init_notebook_mode, iplot, iplot_mpl
+init_notebook_mode()
+
+## load data
+data = pd.read_csv('../input/Data/Stocks/goog.us.txt')
+data['Date'] = pd.to_datetime(data['Date'])
+data = data.set_index('Date')
+print(data.index.min(), data.index.max())
+
+
+## set features
+class Environment1:
+    
+    def __init__(self, data, history_t=90):
+        self.data = data
+        self.history_t = history_t
+        self.reset()
+        
+    def reset(self):
+        self.t = 0
+        self.done = False
+        self.profits = 0
+        self.positions = []
+        self.position_value = 0
+        self.history = [0 for _ in range(self.history_t)]
+        return [self.position_value] + self.history # obs
+    
+    def step(self, act):
+        reward = 0
+        
+
+        if act == 1:
+            self.positions.append(self.data.iloc[self.t, :]['Close'])
+        elif act == 2: 
+            if len(self.positions) == 0:
+                reward = -1
+            else:
+                profits = 0
+                for p in self.positions:
+                    profits += (self.data.iloc[self.t, :]['Close'] - p)
+                reward += profits
+                self.profits += profits
+                self.positions = []
+        
+        # set next time
+        self.t += 1
+        self.position_value = 0
+        for p in self.positions:
+            self.position_value += (self.data.iloc[self.t, :]['Close'] - p)
+        self.history.pop(0)
+        self.history.append(self.data.iloc[self.t, :]['Close'] - self.data.iloc[(self.t-1), :]['Close'])
+        
+        # clipping reward
+        if reward > 0:
+            reward = 1
+        elif reward < 0:
+            reward = -1
+        
+            return [self.position_value] + self.history, reward, self.done
+
+
+
+## load training data	    
+def train_dqn(env):
+
+    class Q_Network(chainer.Chain):
+
+        def __init__(self, input_size, hidden_size, output_size):
+            super(Q_Network, self).__init__(
+                fc1 = L.Linear(input_size, hidden_size),
+                fc2 = L.Linear(hidden_size, hidden_size),
+                fc3 = L.Linear(hidden_size, output_size)
+            )
+
+        def __call__(self, x):
+            h = F.relu(self.fc1(x))
+            h = F.relu(self.fc2(h))
+            y = self.fc3(h)
+            return y
+
+        def reset(self):
+            self.zerograds()
+
+    Q = Q_Network(input_size=env.history_t+1, hidden_size=100, output_size=3)
+    Q_ast = copy.deepcopy(Q)
+    optimizer = chainer.optimizers.Adam()
+    optimizer.setup(Q)
+
+    epoch_num = 50
+    step_max = len(env.data)-1
+    memory_size = 200
+    batch_size = 20
+    epsilon = 1.0
+    epsilon_decrease = 1e-3
+    epsilon_min = 0.1
+    start_reduce_epsilon = 200
+    train_freq = 10
+    update_q_freq = 20
+    gamma = 0.97
+    show_log_freq = 5
+
+    memory = []
+    total_step = 0
+    total_rewards = []
+    total_losses = []
+
+    start = time.time()
+    for epoch in range(epoch_num):
+
+        pobs = env.reset()
+        step = 0
+        done = False
+        total_reward = 0
+        total_loss = 0
+
+        while not done and step < step_max:
+
+            # select act
+            pact = np.random.randint(3)
+            if np.random.rand() > epsilon:
+                pact = Q(np.array(pobs, dtype=np.float32).reshape(1, -1))
+                pact = np.argmax(pact.data)
+
+            # act
+            obs, reward, done = env.step(pact)
+
+            # add memory
+            memory.append((pobs, pact, reward, obs, done))
+            if len(memory) > memory_size:
+                memory.pop(0)
+
+            # train or update q
+            if len(memory) == memory_size:
+                if total_step % train_freq == 0:
+                    shuffled_memory = np.random.permutation(memory)
+                    memory_idx = range(len(shuffled_memory))
+                    for i in memory_idx[::batch_size]:
+                        batch = np.array(shuffled_memory[i:i+batch_size])
+                        b_pobs = np.array(batch[:, 0].tolist(), dtype=np.float32).reshape(batch_size, -1)
+                        b_pact = np.array(batch[:, 1].tolist(), dtype=np.int32)
+                        b_reward = np.array(batch[:, 2].tolist(), dtype=np.int32)
+                        b_obs = np.array(batch[:, 3].tolist(), dtype=np.float32).reshape(batch_size, -1)
+                        b_done = np.array(batch[:, 4].tolist(), dtype=np.bool)
+
+                        q = Q(b_pobs)
+                        maxq = np.max(Q_ast(b_obs).data, axis=1)
+                        target = copy.deepcopy(q.data)
+                        for j in range(batch_size):
+                            target[j, b_pact[j]] = b_reward[j]+gamma*maxq[j]*(not b_done[j])
+                        Q.reset()
+                        loss = F.mean_squared_error(q, target)
+                        total_loss += loss.data
+                        loss.backward()
+                        optimizer.update()
+
+                if total_step % update_q_freq == 0:
+                    Q_ast = copy.deepcopy(Q)
+
+            # epsilon
+            if epsilon > epsilon_min and total_step > start_reduce_epsilon:
+                epsilon -= epsilon_decrease
+
+            # next step
+            total_reward += reward
+            pobs = obs
+            step += 1
+            total_step += 1
+
+        total_rewards.append(total_reward)
+        total_losses.append(total_loss)
+
+        if (epoch+1) % show_log_freq == 0:
+            log_reward = sum(total_rewards[((epoch+1)-show_log_freq):])/show_log_freq
+            log_loss = sum(total_losses[((epoch+1)-show_log_freq):])/show_log_freq
+            elapsed_time = time.time()-start
+            print('\t'.join(map(str, [epoch+1, epsilon, total_step, log_reward, log_loss, elapsed_time])))
+            start = time.time()
+            
+    return Q, total_losses, total_rewards
+    
+
+
+    ## output and plots
+    Q, total_losses, total_rewards = train_dqn(Environment1(train))
+
+    def plot_loss_reward(total_losses, total_rewards):
+
+    figure = tools.make_subplots(rows=1, cols=2, subplot_titles=('loss', 'reward'), print_grid=False)
+    figure.append_trace(Scatter(y=total_losses, mode='lines', line=dict(color='skyblue')), 1, 1)
+    figure.append_trace(Scatter(y=total_rewards, mode='lines', line=dict(color='orange')), 1, 2)
+    figure['layout']['xaxis1'].update(title='epoch')
+    figure['layout']['xaxis2'].update(title='epoch')
+    figure['layout'].update(height=400, width=900, showlegend=False)
+    iplot(figure)
+    
+
+plot_loss_reward(total_losses, total_rewards)
+
