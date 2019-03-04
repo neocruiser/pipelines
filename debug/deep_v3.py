ce23aaaf27ca83e8eed39a17f83e9c7be8813146
neocruiser <slei.bass@gmail.com>
2019-03-04 15:21:36 -0500

    qualimap data mining added

--- debug/deep_v3.py
+++ debug/deep_v3.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+
+
 ## embedding with neural network
 ## keras starter NN with embeddings
 ## lightgbm fixing unbalanced data
--- debug/generative_mod.v1.23.py
+++ debug/generative_mod.v1.23.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+
+
 ##  two-bidirectional-GRU-layer neural net
 
 import time
